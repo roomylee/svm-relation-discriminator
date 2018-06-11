63bbb38ba7f62b692751c0e83d392d47674d5b8b
Joohong <roomylee@naver.com>
2018-06-12 05:34:23 +0900

    add test phase

--- src/Discriminator/Discriminator.java
+++ src/Discriminator/Discriminator.java
@@ -41,79 +41,88 @@ public class Discriminator {
 		svmModel.model = svmModel.load_model(dir);
 	}
 
-	public static List<String> listFilesForDirectory(final File dir){
-		List<String> fileList = new ArrayList<>();
-		for(final File fileEntry : dir.listFiles()){
-			if(fileEntry.isDirectory()){
-				fileList.addAll(listFilesForDirectory(fileEntry));
-			} else{
-				fileList.add(fileEntry.getName());
-			}
-		}
-		return fileList;
-	}
-
 	public static void main(String args[]) {
-		List<String> corpus = new ArrayList<>();
-		List<Integer> label = new ArrayList<>();
 
-		final File dir = new File("data");
-		List<String> fileList = listFilesForDirectory(dir);
+		// Train Phase
+		List<String> trainCorpus = new ArrayList<>();
+		List<Integer> trainLabel = new ArrayList<>();
 
-		for(String fileName : fileList) {
-			FileReader fr;
-			BufferedReader br;
-			try {
-				fr = new FileReader("data" + "/" + fileName);
-				br = new BufferedReader(fr);
+		String trainDir = "data/train.txt";
 
-				String curLine;
-				while ((curLine = br.readLine()) != null) {
-					String x = curLine.split("\t")[0];
-					corpus.add(x);
-
-					String y = curLine.split("\t")[1];
-					if (y.equals("None")){
-						label.add(-1);
-					}
-					else {
-						label.add(+1);
-					}
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-
-		String svm_dir = "model/svm.model";
-		Discriminator d = new Discriminator();
-		d.train(corpus, label);
+		FileReader fr;
+		BufferedReader br;
 		try {
-			d.save_svm(svm_dir);
-		} catch (IOException e) {
+			fr = new FileReader(trainDir);
+			br = new BufferedReader(fr);
+
+			String curLine;
+			while ((curLine = br.readLine()) != null) {
+				String x = curLine.split("\t")[0];
+				trainCorpus.add(x);
+
+				String y = curLine.split("\t")[1];
+				if (y.equals("None")){
+					trainLabel.add(-1);
+				}
+				else {
+					trainLabel.add(+1);
+				}
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+		// Train SVM
+		String svm_dir = "model/svm.model";
+		Discriminator d = new Discriminator();
+		d.train(trainCorpus, trainLabel);
 		try {
+			d.save_svm(svm_dir);
 			d.load_svm(svm_dir);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		List<Double> pred = d.predict(corpus);
+
+		// Test(Evaluate) Phase
+		List<String> testCorpus = new ArrayList<>();
+		List<Integer> testLabel = new ArrayList<>();
+		String testDir = "data/test.txt";
+
+		try {
+			fr = new FileReader(testDir);
+			br = new BufferedReader(fr);
+
+			String curLine;
+			while ((curLine = br.readLine()) != null) {
+				String x = curLine.split("\t")[0];
+				testCorpus.add(x);
+
+				String y = curLine.split("\t")[1];
+				if (y.equals("None")){
+					testLabel.add(-1);
+				}
+				else {
+					testLabel.add(+1);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		List<Double> pred = d.predict(testCorpus);
 		int tp=0, tn=0, fp=0, fn=0;
 		double threshold=0.5;
 		for(int i=0; i<pred.size(); i++)
 		{
 			if(pred.get(i) > threshold) {
-				if(label.get(i) == 1)
+				if(testLabel.get(i) == 1)
 					++tp;
 				else
 					++fp;
 			}
 			else {
-				if(label.get(i) == -1)
+				if(testLabel.get(i) == -1)
 					++tn;
 				else
 					++fn;
@@ -126,6 +135,6 @@ public class Discriminator {
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
 
 		d.svmModel.param.probability = 0;
-		d.cross_validation(corpus, label, 10);
+		d.cross_validation(trainCorpus, trainLabel, 10);
 	}
 }
